9127167ef8bb5c3b1502ab2f7fc9f30e52117dfd
rafalswirk <rafalswirk@outlook.com>
2023-04-08 19:31:06 +0200

    - Setting test for finding specific element in json

--- MoneyKeeper.Tests/GCloudOcrDataReaderTests.cs
+++ MoneyKeeper.Tests/GCloudOcrDataReaderTests.cs
@@ -1,11 +1,34 @@
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text.Json;
+
 namespace MoneyKeeper.Tests
 {
     public class GCloudOcrDataReaderTests
     {
         [Fact]
-        public void Test1()
+        public void FindDataInGCloudJsonFile()
         {
-            throw new NotImplementedException();
+            int foundElements = 0;
+            var jsonDocument = JsonDocument.Parse(File.OpenRead(@"TestData/BillsOfSale/GCloud/1_gcloud.json"));
+            
+            var textAnnotations = jsonDocument
+                .RootElement
+                .GetProperty("responses")
+                .EnumerateArray()
+                .First()
+                .GetProperty("textAnnotations")
+                .EnumerateArray();
+            foreach (var annotation in textAnnotations)
+            {
+                if (annotation.TryGetProperty("description", out JsonElement description))
+                {
+                    if(description.ToString() == "SUMA")
+                        foundElements++;
+                }
+            }
+
+            Assert.Equal(2, foundElements);
         }
     }
 }
