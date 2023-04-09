aba36b7914fef2d16f78b54f0f478cc4734cc7e5
rafalswirk <rafalswirk@outlook.com>
2023-04-09 06:11:42 +0200

    - Moving json document creation to test constructor

---
+++ MoneyKeeper.OCR/GCloud/TextAnnotatioinsParser.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Text.Json;
+
+namespace MoneyKeeper.OCR.GCloud
+{
+    public record TextAnnotation(string Description, Rectangle BoundingPoly);
+
+    public class TextAnnotationParser
+    {
+        public IReadOnlyCollection<TextAnnotation> Parse(JsonDocument document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MoneyKeeper.Tests/GCloudOcrDataReaderTests.cs
+++ MoneyKeeper.Tests/GCloudOcrDataReaderTests.cs
@@ -1,3 +1,4 @@
+using MoneyKeeper.OCR.GCloud;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Text.Json;
@@ -6,19 +7,26 @@ namespace MoneyKeeper.Tests
 {
     public class GCloudOcrDataReaderTests
     {
+        private readonly JsonDocument _jsonDocument;
+
+        public GCloudOcrDataReaderTests()
+        {
+            _jsonDocument = JsonDocument.Parse(File.OpenRead(@"TestData/BillsOfSale/GCloud/1_gcloud.json"));
+        }
+
         [Fact]
         public void FindDataInGCloudJsonFile()
         {
             int foundElements = 0;
-            var jsonDocument = JsonDocument.Parse(File.OpenRead(@"TestData/BillsOfSale/GCloud/1_gcloud.json"));
-            
-            var textAnnotations = jsonDocument
+
+            var textAnnotations = _jsonDocument
                 .RootElement
                 .GetProperty("responses")
                 .EnumerateArray()
                 .First()
                 .GetProperty("textAnnotations")
                 .EnumerateArray();
+
             foreach (var annotation in textAnnotations)
             {
                 if (annotation.TryGetProperty("description", out JsonElement description))
@@ -29,6 +37,14 @@ namespace MoneyKeeper.Tests
             }
 
             Assert.Equal(2, foundElements);
+        }
+
+        [Fact]
+        public void ParseGCloudData()
+        {
+            var parser = new TextAnnotationParser();
+            var annotations = parser.Parse(_jsonDocument);
+
         }
     }
 }
