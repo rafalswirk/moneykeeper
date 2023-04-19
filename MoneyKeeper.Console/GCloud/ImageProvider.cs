f6800816d806f5ed20bde7ffea840cd07f702ee8
rafalswirk <rafalswirk@outlook.com>
2023-04-19 16:59:47 +0200

    - Added console app for sending image to gcloud and reading resoults

---
+++ MoneyKeeper.Console/GCloud/ImageProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Console.GCloud
+{
+    internal class ImageProvider
+    {
+        string jsonContent = "{\n  \"requests\": [\n    {\n      \"image\": {\n        \"content\": \"base64_image_placeholder\"\n      },\n      \"features\": [\n        {\n          \"type\": \"TEXT_DETECTION\"\n        }\n      ]\n    }\n  ]\n}";
+        public async Task<string> SendImage(string imagePath, string token, string projectId)
+        {
+            var base64Image = GetBase64(imagePath);
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://vision.googleapis.com/v1/images:annotate"),
+                Headers =
+                    {
+                        { "Authorization", token },
+                        { "x-goog-user-project", projectId },
+                    },
+                Content = new StringContent(jsonContent.Replace("base64_image_placeholder", base64Image))
+                {
+                    Headers =
+                    {
+                        ContentType = new MediaTypeHeaderValue("application/json")
+                    }
+                }
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                System.Console.WriteLine(body);
+                return body;
+            }
+        }
+
+        public string GetBase64(string imagePath) 
+        {
+            var image = Image.Load(File.ReadAllBytes(imagePath));
+            using (MemoryStream m = new MemoryStream())
+            {
+                image.Save(m, image.Metadata.DecodedImageFormat);
+                byte[] imageBytes = m.ToArray();
+
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+    }
+}
---
+++ MoneyKeeper.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+using MoneyKeeper.OCR.GCloud;
+
+namespace MoneyKeeper.Console
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+            var token = configuration.GetSection("GCloud:AccessToken");
+            var projectId = configuration.GetSection("GCloud:ProjectId");
+
+            var imageProvider = new GCloud.ImageProvider();
+            var filePath = @"d:\Paragony\IMG_20230418_102305232~2.jpg";
+            var jsonFileName = $"gcloud-{Path.GetFileNameWithoutExtension(filePath)}.json";
+            var gloudJson = await imageProvider.SendImage(filePath, $"Bearer {token.Value}", projectId.Value);
+            File.WriteAllText("gcloud-IMG_20230418_102305232~2.json", gloudJson); //paragon2
+            
+            var parser = new BillOfSaleParser();
+            var result = parser.Parse(File.ReadAllText(jsonFileName));
+
+            System.Console.WriteLine(result.ToString());
+            System.Console.WriteLine("Program finished!");
+        }
+    }
+}
