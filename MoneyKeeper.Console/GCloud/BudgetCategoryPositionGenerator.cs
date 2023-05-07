e04f262bf77fd952204b4812cb058b8181407206
rafalswirk <rafalswirk@outlook.com>
2023-05-07 06:32:55 +0200

    - Preaparing tests for category position calculations

---
+++ MoneyKeeper.Console/GCloud/BudgetCategoryPositionGenerator.cs
@@ -0,0 +1,19 @@
+﻿using MoneyKeeper.Budget.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Console.GCloud
+{
+    internal class BudgetCategoryPositionGenerator
+    {
+        public IReadOnlyCollection<CategorySpreadsheetMap> Generate(IReadOnlyCollection<BudgetCategory> categories, IReadOnlyCollection<string> rawData, int firstRowNumber)
+        {
+            var categorySpreadsheetMap = new List<CategorySpreadsheetMap>();
+            
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MoneyKeeper.Tests/Budget.Tests/BudgetCategoryPositionGeneratorTests.cs
@@ -0,0 +1,45 @@
+﻿using MoneyKeeper.Budget.Entities;
+using MoneyKeeper.Console.GCloud;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Tests.Budget.Tests
+{
+    public class BudgetCategoryPositionGeneratorTests
+    {
+        [Fact]
+        public void Generate_SpreadsheetRawData_ReturnsBudgetCategoryPosition()
+        {
+            int firstRowNumber = 10;
+            var input = new List<string>
+            {
+                "Telekomunikacja",
+                "Telefon 1",
+                "TV",
+                ".",
+                " ",
+                "Opieka zdrowotna",
+                "Lekarz",
+                "Inne",
+                ""
+            };
+            var categories = new List<BudgetCategory>()
+            {
+                new BudgetCategory { Category = "Telefon 1", Group = "Telekomunikacja"},
+                new BudgetCategory { Category = "TV", Group = "Telekomunikacja"},
+                new BudgetCategory { Category = "Lekarz", Group = "Opieka zdrowotna"},
+                new BudgetCategory { Category = "Inne", Group = "Opieka zdrowotna"}
+            };
+            var generator = new BudgetCategoryPositionGenerator();
+            var mappings = generator.Generate(categories, input, firstRowNumber);
+
+            Assert.NotNull(mappings);
+            Assert.Equal(4, mappings.Count);
+            Assert.True(mappings.Single(m => m.Category.Category == "Inne" && m.Category.Group == "Opieka zdrowotna").Row == "17");    
+            Assert.True(mappings.Single(m => m.Category.Category == "TV").Row == "12");    
+        }
+    }
+}
