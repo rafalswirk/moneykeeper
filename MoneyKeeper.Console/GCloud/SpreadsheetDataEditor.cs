e6ff91e5da970f7cd487d26f98e19b5f84f26d58
Rafał Świrk <rafalswirk@outlook.com>
2023-05-19 00:07:27 +0200

    - Added basic logic with test for generating cell data

---
+++ MoneyKeeper.Console/GCloud/SpreadsheetDataEditor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Console.GCloud
+{
+    internal class SpreadsheetDataEditor
+    {
+        public string Add(string cellContent, string value)
+        {
+            if(string.IsNullOrEmpty(cellContent))
+            {
+                return value;
+            }
+            if (cellContent.StartsWith("="))
+            {
+                return cellContent + $"+ {value}";
+            }
+            return $"={cellContent}+{value}";
+        }
+    }
+}
