24918a31f15b9773e020708e249f258546f2f173
Rafał Świrk <rafalswirk@outlook.com>
2023-05-24 11:15:23 +0200

    - Added ReceiptInfo data classes

---
+++ src/Server/Modules/Budget/MoneyKeeper.Budget.Core/Entities/ReceiptInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Budget.Core.Entities
+{
+    public class ReceiptInfo
+    {
+        public int Id { get; set; }
+        public string ImageName { get; set; }
+        public bool OcrDataGenerated { get; set; }
+        public bool OcrValidationResult { get; set; }
+        public bool SpreadsheetEntered { get; set; }
+    }
+}
