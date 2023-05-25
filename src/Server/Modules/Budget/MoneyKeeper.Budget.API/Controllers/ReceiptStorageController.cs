4f7a2e03af449d0bd9c212ffc3ce340ffe386dca
Rafał Świrk <rafalswirk@outlook.com>
2023-05-25 08:42:49 +0200

    - Preparing controller for receipt upload and store

---
+++ src/Server/Modules/Budget/MoneyKeeper.Budget.API/Controllers/ReceiptStorageController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoneyKeeper.Budget.Core.Services;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Budget.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/receiptstorage")]
+    public class ReceiptStorageController : ControllerBase
+    {
+        private readonly string _imageDirectoryPath;
+        private readonly RecepitStorage _receiptStorage;
+
+        public ReceiptStorageController(RecepitStorage receiptStorage)
+        {
+            _receiptStorage = receiptStorage;
+
+            _imageDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images");
+            if (!Directory.Exists(_imageDirectoryPath))
+            {
+                Directory.CreateDirectory(_imageDirectoryPath);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            try 
+            {
+                var info = await _receiptStorage.SaveReceipt(file);
+
+                // Return the URL of the saved image
+                var imageUrl = $"{Request.Scheme}://{Request.Host}/api/images/{info.ImageName}";
+                return Ok(new { imageUrl });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{fileName}")]
+        public IActionResult Get(string fileName)
+        {
+            // Create the full path to the requested image file
+            var filePath = Path.Combine(_imageDirectoryPath, fileName);
+
+            // Check if the file exists
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            // Return the image file
+            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            return new FileStreamResult(fileStream, "image/jpeg");
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetImageUrls()
+        {
+            // Get the list of image files
+            var imageFiles = Directory.GetFiles(_imageDirectoryPath);
+            // Create a list of image URLs
+            var imageUrls = new List<string>();
+            foreach (var imageFile in imageFiles)
+            {
+                var fileName = Path.GetFileName(imageFile);
+                var imageUrl = $"{Request.Scheme}://{Request.Host}/api/images/{fileName}";
+                imageUrls.Add(imageUrl);
+            }
+            return Ok(imageUrls);
+        }
+
+        [HttpGet("isalive")]
+        public IActionResult IsAlive()
+        {
+            return Ok();
+        }
+    }
+
+}
