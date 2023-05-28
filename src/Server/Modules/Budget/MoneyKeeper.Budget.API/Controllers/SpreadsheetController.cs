e7243381879640454f975e2bf36d35753d076d35
Rafał Świrk <rafalswirk@outlook.com>
2023-05-28 12:21:22 +0200

    - Preparing controller for handling spreadsheet operations

---
+++ src/Server/Modules/Budget/MoneyKeeper.Budget.API/Controllers/SpreadsheetController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyKeeper.Budget.API.Controllers
+{
+    [ApiController]
+    [Route("api/receipt/spreadsheet")]
+    public class SpreadsheetController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetValue()
+        {
+            return Ok();
+        }
+    }
+}
