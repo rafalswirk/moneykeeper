86acb1c1bae1b23f9b0a0c7c1d07b590b7808e4c
Rafał Świrk <rafalswirk@outlook.com>
2023-06-05 13:10:00 +0200

    - Creating controller for budget categories

---
+++ src/Server/Modules/Budget/MoneyKeeper.Budget.Core/Services/CategoriesService.cs
@@ -0,0 +1,36 @@
+﻿using MoneyKeeper.Budget.DTO;
+using MoneyKeeper.Budget.Entities;
+using MoneyKeeper.Budget.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Budget.Core.Services
+{
+    public class CategoriesService
+    {
+        private readonly IBudgetCategoryRepository _budgetCategoryRepository;
+
+        public CategoriesService(IBudgetCategoryRepository budgetCategoryRepository)
+        {
+            _budgetCategoryRepository = budgetCategoryRepository;
+        }
+
+        public async Task<IReadOnlyList<BudgetCategoryDto>> BrowseAsync()
+        {
+            var categories = await _budgetCategoryRepository.BrowseAsync();
+            return categories.Select(Map<BudgetCategoryDto>).ToList();
+        }
+
+        private static T Map<T>(BudgetCategory map) where T : BudgetCategoryDto, new()
+            => new T()
+            {
+                Id = map.Id,
+                Category = map.Category,
+                Group = map.Group,
+                Comment = map.Comment,
+            };
+    }
+}
