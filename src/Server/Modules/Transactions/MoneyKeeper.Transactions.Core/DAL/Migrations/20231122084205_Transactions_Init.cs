d119b1153327508e1bc29a70788b7aeb7d993b1d
rafalswirk <rafalswirk@outlook.com>
2023-11-22 09:42:45 +0100

    - Added migration for transactions database

---
+++ src/Server/Modules/Transactions/MoneyKeeper.Transactions.Core/DAL/Migrations/20231122084205_Transactions_Init.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyKeeper.Transactions.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Transactions_Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReceiptInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ImageName = table.Column<string>(type: "TEXT", nullable: false),
+                    OcrDataGenerated = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    OcrValidationResult = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    SpreadsheetEntered = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    UploadDate = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceiptInfos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReceiptInfos");
+        }
+    }
+}
