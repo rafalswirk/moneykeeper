79647fc87730f05c1adcb0c4c5beb5bfe1594b47
Rafał Świrk <rafalswirk@outlook.com>
2023-11-24 10:47:48 +0100

    - Marking entities in ReceiptInfo as boolean

--- src/Server/Modules/Transactions/MoneyKeeper.Transactions.Core/Entities/ReceiptInfo.cs
+++ src/Server/Modules/Transactions/MoneyKeeper.Transactions.Core/Entities/ReceiptInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,11 @@ namespace MoneyKeeper.Transactions.Core.Entities
     {
         public int Id { get; set; }
         public string ImageName { get; set; }
+        [Column(TypeName = "boolean")]
         public bool OcrDataGenerated { get; set; }
+        [Column(TypeName = "boolean")]
         public bool? OcrValidationResult { get; set; }
+        [Column(TypeName = "boolean")]
         public bool SpreadsheetEntered { get; set; }
         public DateOnly UploadDate { get; set; }
     }
