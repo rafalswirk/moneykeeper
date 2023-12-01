b0c7326a8a271f88ba98c7e245bca4358508fe09
Rafał Świrk <rafalswirk@outlook.com>
2023-12-01 18:48:02 +0100

    - Showing receipt preview in details view

--- src/Client/MoneyKeeper.Client/View/ReceiptDetails.xaml.cs
+++ src/Client/MoneyKeeper.Client/View/ReceiptDetails.xaml.cs
@@ -4,12 +4,20 @@ namespace MoneyKeeper.Client.View;
 
 public partial class ReceiptDetails : ContentPage
 {
+    private const string BaseApiUrl = "http://localhost:5126/api/";
+    private readonly string ImagesApiUrl = $"{BaseApiUrl}images/";
+    private readonly string ReceiptApiUrl = $"{BaseApiUrl}receipt/storage";
+    private readonly string CategoriesApiUrl = $"{BaseApiUrl}budget/categories";
+
     public ReceiptInfoDto Info { get; }
 
 	public ReceiptDetails(DTO.ReceiptInfoDto info)
 	{
         Info = info;
 		InitializeComponent();
+        string apiUrl = $"{BaseApiUrl}images/{Info.ImageName}";
+
+        imagePreview.Source = ImageSource.FromUri(new Uri(apiUrl));
     }
 
 }
