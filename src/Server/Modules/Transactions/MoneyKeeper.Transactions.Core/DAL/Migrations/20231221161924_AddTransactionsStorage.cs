420bbf0417f57c7a593b0ff9d572b320234bf439
Rafał Świrk <rafalswirk@outlook.com>
2023-12-21 17:20:32 +0100

    - Generating migration for transactions storage

---
+++ src/Server/Modules/Transactions/MoneyKeeper.Transactions.Core/DAL/Migrations/20231221161924_AddTransactionsStorage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyKeeper.Transactions.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTransactionsStorage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "UploadDate",
+                table: "ReceiptInfos",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "ReceiptInfos",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ReceiptInfos",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "UploadDate",
+                table: "ReceiptInfos",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "ReceiptInfos",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ReceiptInfos",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
