c9a0a1fce8f04ffae8bdbbefde9d1aae09569ffa
Rafał Świrk <rafalswirk@outlook.com>
2023-12-22 12:56:43 +0100

    - Writing information about entering value to spreadsheet

--- src/Client/MoneyKeeper.Client/Core/Backend/TransactionCommit.cs
+++ src/Client/MoneyKeeper.Client/Core/Backend/TransactionCommit.cs
@@ -9,14 +9,14 @@ using System.Threading.Tasks;
 
 namespace MoneyKeeper.Client.Core.Backend
 {
+    //Todo Make communication between modules at server side, so from client perspective there is only post transaction
     internal class TransactionCommit
     {
         public async Task CommitTransactionAsync(TransactionData transaction)
         {
-            await Task.WhenAll(
-                Consts.BaseApiUrl.AppendPathSegment("budget/transaction").PostJsonAsync(new TransactionDto(transaction.Date, transaction.CategoryId, transaction.Value)),
-                Consts.BaseApiUrl.AppendPathSegment("transactions").PostJsonAsync(new TransactionStoreDto(transaction.Value, transaction.Date, 1))
-            );
+            await Consts.BaseApiUrl.AppendPathSegment("budget/transaction").PostJsonAsync(new TransactionDto(transaction.Date, transaction.CategoryId, transaction.Value));
+            await Consts.BaseApiUrl.AppendPathSegment("transactions/spreadsheetEntered").PatchJsonAsync(new {id = transaction.ReceiptId });
+            await Consts.BaseApiUrl.AppendPathSegment("transactions").PostJsonAsync(new TransactionStoreDto(transaction.Value, transaction.Date, 1));
         }
     }
 }
