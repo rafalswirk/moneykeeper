a69911a51b0e8adc85ef85a430e0e47f4632f938
Rafał Świrk <rafalswirk@outlook.com>
2024-01-22 08:00:16 +0100

    - Creating entity for storing different years

---
+++ src/Server/Modules/Budget/MoneyKeeper.Budget.Core/Entities/Spreadsheets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Budget.Core.Entities
+{
+    public class Spreadsheets
+    {
+        public int Id { get; set; }
+        public string SpreadsheetKey { get; set; }
+        public int Year { get; set; }
+    }
+}
