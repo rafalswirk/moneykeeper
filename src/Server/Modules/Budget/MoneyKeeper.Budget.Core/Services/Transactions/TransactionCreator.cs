cabce99594cd25b922e102899077a93c60882d54
Rafał Świrk <rafalswirk@outlook.com>
2024-01-23 05:55:12 +0100

    - Creating basic structures for transaction creation tests

---
+++ src/Server/Modules/Budget/MoneyKeeper.Budget.Core/Services/Transactions/TransactionCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper.Budget.Core.Services.Transactions
+{
+    public class TransactionCreator
+    {
+        internal void Create(DTO.TransactionDto transactionDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Server/Tests/MoneyKeeper.Tests/Budget.Tests/TransactionsTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using MoneyKeeper.Budget.Core.Services.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneyKeeper.Budget.Core.DTO;
+
+namespace MoneyKeeper.UnitTests.Budget.Tests
+{
+    public class TransactionsTestFixture
+    {
+        public void Create_TransactionFor2024_CreatingTransactionInRepositoryFor2024Year()
+        {
+            var creator = new TransactionCreator();
+            creator.Create(new TransactionDto(new DateTime(2024, 4, 22), 1, 22.4));
+        }
+    }
+}
