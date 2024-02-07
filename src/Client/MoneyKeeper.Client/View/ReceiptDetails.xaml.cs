ba0f32b64e3fd6b8c0f8db0fead5c58c27b33a5b
Rafał Świrk <rafalswirk@outlook.com>
2024-02-07 17:15:25 +0100

    - Generating  date with noon hour

--- src/Client/MoneyKeeper.Client/View/ReceiptDetails.xaml.cs
+++ src/Client/MoneyKeeper.Client/View/ReceiptDetails.xaml.cs
@@ -33,9 +33,9 @@ public partial class ReceiptDetails : ContentPage
             var category = (pckCategories.SelectedItem as BudgetCategoryDto);
             var transaction = new TransactionCommit();
             await transaction.CommitTransactionAsync(
-                new TransactionData(Value, dpTransactionDate.Date, category.Id, Info.Id));
+                new TransactionData(Value, SetDateWithDefaultHour(), category.Id, Info.Id));
         }
-        catch(MoneyKeeperException)
+        catch (MoneyKeeperException)
         {
             await DisplayAlert("Error", "Error during transaction creation!", "OK");
         }
@@ -49,6 +49,12 @@ public partial class ReceiptDetails : ContentPage
             activIndicator.IsVisible = false;
             activIndicator.IsRunning = false;
         }
+    }
+
+    private DateTime SetDateWithDefaultHour()
+    {
+        return new DateTime(dpTransactionDate.Date.Year, dpTransactionDate.Date.Month, dpTransactionDate.Date.Day,
+            12, 0, 0, DateTimeKind.Local);
     }
 
     private async void control_Appearing(object sender, EventArgs e)
