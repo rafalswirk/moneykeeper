ca4db1d082117b4219f781e5a2fc519a420225fb
Rafał Świrk <rafalswirk@outlook.com>
2025-01-07 18:11:03 +0100

    - Implement test case

--- src/Server/Tests/MoneyKeeper.Tests/Transactions.Tests/Storage/ReceiptRemoveTestFixture.cs
+++ src/Server/Tests/MoneyKeeper.Tests/Transactions.Tests/Storage/ReceiptRemoveTestFixture.cs
@@ -1,11 +1,11 @@
 ﻿using FakeItEasy;
-using Microsoft.Extensions.Logging;
 using MoneyKeeper.Transactions.Core.Entities;
 using MoneyKeeper.Transactions.Core.Repositories;
 using MoneyKeeper.Transactions.Core.Storage;
 using MoneyKeeper.UnitTests.Mocks;
 using System;
 using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +17,22 @@ namespace MoneyKeeper.UnitTests.Transactions.Tests.Storage
         [Fact]
         public async Task Remove_WithValidReceiptInfo_RemovingReceiptFromHardDriveAndDatabase()
         {
+            var fileName = $"{Guid.NewGuid()}.jpg";
+            var fileSystem = new MockFileSystem();
+            fileSystem.AddEmptyFile($"C:\\mk-data\\Receipts\\{fileName}");
             var repository = new InMemoryReceiptRepository();
-            var logger = A.Fake(ILogger);
+            repository.AddAsync(new ReceiptInfo 
+            {
+                ImageName = fileName,
+            });
+            var logger = A.Fake<Microsoft.Extensions.Logging.ILogger<ReceiptRemove>>();
             var storageOperation = new ReceiptRemove(repository, logger);
-            await storageOperation.Remove(new ReceiptInfo());
 
-            throw new NotImplementedException();
+            await storageOperation.Remove(new ReceiptInfo());
+            
+            Assert.Equal(0, fileSystem.Directory.GetFiles(@"C:\mk-data\Receipts").Length);
+            var receipts = await repository.BrowseAsync();
+            Assert.Equal(0, receipts.Where(r => r.ImageName == fileName).Count());
         }
 
         [Fact]
